d55d53a014ef6697c25dcfb6d92393318276f82f
Helene Gaspar <helene.gaspar@velopass.com>
2025-09-25 14:29:48 +0200

    created index page

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,6 @@ class Item < ApplicationRecord
   has_many :order_items
   has_many :orders, through: :order_items
 
+  has_one_attached :header_photo   # main image
   has_many_attached :photos
 end
