75e501df5ee8abe4d4d74d5bb453067198b15ec0
Helene Gaspar <helene.gaspar@velopass.com>
2025-09-25 13:59:36 +0200

    set routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :items, only: [:index, :show]
   resources :orders, only: [:new, :create] # checkout flow
+  post "/webhooks/stripe", to: "webhooks#stripe"
+
   root "items#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
