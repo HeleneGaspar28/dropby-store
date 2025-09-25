d9c7bba0010266206622346c424e27f55766a254
Helene Gaspar <helene.gaspar@velopass.com>
2025-09-25 13:47:13 +0200

    added orderitem model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_25_114008) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_25_114654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -56,6 +56,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_25_114008) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.string "customer_name"
     t.string "customer_email"
@@ -71,4 +80,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_25_114008) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
 end
